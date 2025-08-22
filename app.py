94b63bd19d9c6ecb5e4c9ee92b7d5f68f55256db
pabitramondalkgpian25 <pabitramondal2525@kgpian.iitkgp.ac.in>
2025-08-22 20:49:12 +0530

    app.py

--- app.py
+++ app.py
@@ -149,7 +149,7 @@ with right:
                               "- Result shows ✅ for REAL and ❌ for FAKE.\n"
                               "- Thank you for using this webpage ."
                           )
-    st.info("Tip: For best accuracy, use the **news from the dataset**")
+
 
 
 
