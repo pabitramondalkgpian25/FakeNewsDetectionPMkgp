edb7c9b81577456bbad0c7248d0b73d66068c27f
pabitramondalkgpian25 <pabitramondal2525@kgpian.iitkgp.ac.in>
2025-09-01 07:53:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -133,10 +133,10 @@ with left:
                 st.markdown("<div class='result'>", unsafe_allow_html=True)
                 if is_real:
                    st.success("✅ REAL news detected")
-                   st.markdown(f"<div class='confidence'>Confidence (REAL): {p_real*100:.1f}%</div>", unsafe_allow_html=True)
+                   st.markdown(f"<div class='confidence'>Logistic Regression Probablity : {p_real:.3f}%</div>", unsafe_allow_html=True)
                 else:
                    st.error("❌ FAKE news detected")
-                   st.markdown(f"<div class='confidence'>Confidence (REAL): {p_real*100:.1f}%</div>", unsafe_allow_html=True)
+                   st.markdown(f"<div class='confidence'>Logistic Regression Probablity : {p_real:.3f}%</div>", unsafe_allow_html=True)
                    st.markdown("</div>", unsafe_allow_html=True)
 
             except Exception as e:
@@ -149,6 +149,7 @@ with right:
                               "- Result shows ✅ for REAL and ❌ for FAKE.\n"
                               "- Thank you for using this webpage ."
                           )
+
 
 
 
